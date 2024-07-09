3310ffe04e7e767cd0a0683bdb5f8f31fdb6b2ed
keerekeerweere <daniel.bram@compea.com>
2024-07-09 20:56:17 +0200

    updated default config values

--- src/Config_example.h
+++ src/Config_example.h
@@ -18,30 +18,32 @@
 // Then use a browser to configure the other parameters once the device is connected to the network
 // ------------------------------------------------------------------------------------------------
 
+//#define USE_DEFINE_CONFIG 
 
+#ifdef USE_DEFINE_CONFIG
 
 // SMA login password for UG_USER or UG_INSTALLER always 12 char. Unused=0x00
-// #define USERGROUP UG_USER
+#define USERGROUP UG_USER
 
-// #define WIFI_SSID "ssid"
-// #define WIFI_PASSWORD "ssidpassword"
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "ssidpassword"
 
-// #define MQTT_BROKER "192.168.0.100"
-// #define MQTT_PORT  1883
-// #define MQTT_USER  "mqttuser"
-// #define MQTT_PASS  "mqttpass"
-// #define MQTT_topic "SMA"
-// #define SMA_PASS  "inverterpass"
-// #define SMA_BTADDRESS "00:80:25:00:00:00"
-// #define SCAN_RATE  60
-// #define HASS_DISCOVERY false
-// #define HASS_STATE_TOPIC "homeassistant/sensor/%s/state"
-// #define TIMEZONE 1
-// #define NTPHOSTNAME "pool.ntp.org"
-// #define SUNUP 6
-// #define SUNDOWN 18
-// #define NIGHTSCANRATE 15*60*1000
+#define MQTT_BROKER "192.168.0.100"
+#define MQTT_PORT  1883
+#define MQTT_USER  "mqttuser"
+#define MQTT_PASS  "mqttpass"
+#define MQTT_topic "SMA"
+#define SMA_PASS  "inverterpass"
+#define SMA_BTADDRESS "00:80:25:00:00:00"
+#define SCAN_RATE  60
+#define HASS_DISCOVERY false
+#define HASS_STATE_TOPIC "homeassistant/sensor/%s/state"
+#define TIMEZONE 1
+#define NTPHOSTNAME "pool.ntp.org"
+#define SUNUP 6
+#define SUNDOWN 18
+#define NIGHTSCANRATE 15*60*1000
 
-
+#endif
 
 #endif
